458badc8d7f85dd18add1f38aa41b6d0a2224e4d
farooq13 <farukidris19@gmail.com>
2025-02-09 20:38:07 +0100

    add new features

--- GSU/main.c
+++ GSU/main.c
@@ -6,10 +6,22 @@ int maxNumber(int x, int y) {
 }
 
 
+// int main() {
+//   // int max = maxNumber(3, 5);
+
+//   // printf("%d", max);
+
+  
+
+//   return 0;
+// }
+
+
 int main() {
-  int max = maxNumber(3, 5);
-
-  printf("%d", max);
-
+  for (int i = 20; i <= 50; i++) {
+    if (i == 21 || i == 37)
+    continue;
+    printf("%d\n", i);
+  }
   return 0;
 }
---
+++ increment.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+  // int i = 0;
+  // while (++i < 5) {
+  //   printf("%d\n", i);
+  // }
+
+  // for (int i = 0; i < 5; i++) {
+  //   printf("%d\n", i);
+  // }
+
+  int a = 3;
+
+  if (a == 1) {
+    printf("%d", a);
+  } else {
+    printf("0");
+  }
+
+  //  int result = (a == 1 ? 1 : 0);
+  //  printf("%d", result);
+
+  return 0;
+}
